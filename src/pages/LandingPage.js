9c51490c62b44e07b9beb0a457de715aa80b9183
PatrickMiller164 <millerpatrickjames@gmail.comm>
2024-11-21 10:22:45 +0000

    Testing for autoupdate to Amplify

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -28,7 +28,7 @@ const LandingPage = () => {
         <div className="container mt-5 mb-5">
             <div className="row justify-content-center">
                 <div className="col-md-6 col-lg-6">
-                    <h1 className="mb-4 text-center">Welcome to Playlists by Patrick (TEST2)</h1>
+                    <h1 className="mb-4 text-center">Welcome to Playlists by Patrick</h1>
                     {spotifyAuthUrl ? (
                         <div className="text-center">
                             <p>
